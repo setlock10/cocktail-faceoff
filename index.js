abd6b6694cfddf47cda3e831ba71e94978a2aaa4
John J Setlock II <johnsetlock@gmail.com>
2022-07-30 16:18:56 -0500

    progress

--- index.js
+++ index.js
@@ -14,12 +14,12 @@ const pDrink2=document.querySelector('#cocktail-2-recipe')
 
 //  page load
 document.addEventListener('DOMContentLoaded',()=>{
-    getDrink(imgDrink1,h2Drink1)
-    getDrink(imgDrink2,h2Drink2)
+    getDrink(imgDrink1,h2Drink1,pDrink1)
+    getDrink(imgDrink2,h2Drink2,pDrink2)
  })
 
 // Fetch a drink
-function getDrink(img,h2){
+function getDrink(img,h2,p){
 
     fetch(url)
         .then(res=>res.json())
@@ -27,8 +27,11 @@ function getDrink(img,h2){
             //drink=data.drinks[0]
             console.log(data.drinks[0])
             img.src=data.drinks[0].strDrinkThumb
-            img.width=400
+            img.width=360
             h2.textContent=data.drinks[0].strDrink
+            //p.textContent=`${data.drinks[0].strMeasure1} ${data.drinks[0].strIngredient1}`+"<br>" +
+             //             `${data.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}`
+           
         })
         .catch(e=>console.error(e))
 }
